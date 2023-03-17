cb3deda460ae857363899ff59f5767ca6e050e4a
mobbexLuis <luis.scarrone@mobbex.com>
2023-03-17 09:26:00 -0300

    add getOptions() method

--- mobbex_subscriptions/classes/Helper.php
+++ mobbex_subscriptions/classes/Helper.php
@@ -65,4 +65,44 @@ class Helper
         $cart_id = \Db::getInstance()->getValue('SELECT cart_id FROM ' . _DB_PREFIX_ . "mobbex_subscriber WHERE uid = '$uid'");
         return $cart_id ? new \MobbexSubscriber($cart_id) : null;
     }
+
+     public static function getOptions()
+    {
+        $custom_logo = \Configuration::get(\Mobbex\PS\Checkout\Models\Helper::K_THEME_LOGO);
+
+        // If store's logo option is disabled, use the one configured in mobbex
+        $default_logo = null;
+        if (!empty(\Configuration::get(\Mobbex\PS\Checkout\Models\Helper::K_THEME_SHOP_LOGO))) {
+            $default_logo = \Tools::getShopDomainSsl(true, true) . _PS_IMG_ . \Configuration::get('PS_LOGO');
+        }
+
+        $theme_background = \Configuration::get(\Mobbex\PS\Checkout\Models\Helper::K_THEME_BACKGROUND);
+        $theme_primary = \Configuration::get(\Mobbex\PS\Checkout\Models\Helper::K_THEME_PRIMARY);
+
+        $theme = array(
+            "type" => \Configuration::get(\Mobbex\PS\Checkout\Models\Helper::K_THEME) ?: \Mobbex\PS\Checkout\Models\Helper::K_DEF_THEME,
+            "header" => [
+                "name" => \Configuration::get('PS_SHOP_NAME'),
+                "logo" => !empty($custom_logo) ? $custom_logo : $default_logo,
+            ],
+            'background' => !empty($theme_background) ? $theme_background : null,
+            'colors' => [
+                'primary' => !empty($theme_primary) ? $theme_primary : null,
+            ],
+        );
+
+        $options = array(
+            'button' => (\Configuration::get(\Mobbex\PS\Checkout\Models\Helper::K_EMBED) == true),
+            'domain' => \Context::getContext()->shop->domain,
+            "theme" => $theme,
+            // Will redirect automatically on Successful Payment Result
+            "redirect" => [
+                "success" => true,
+                "failure" => false,
+            ],
+            "platform" => \Mobbex\PS\Checkout\Models\Helper::getPlatform(),
+        );
+
+        return $options;
+    }
 }
